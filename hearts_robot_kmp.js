ed0a7732d6275a54c957578504f30ef6c54de07c
Saman Sahebi <89431215+ssahebi63@users.noreply.github.com>
2024-03-27 22:20:28 -0400

    final version... again...

--- hearts_robot_kmp.js
+++ hearts_robot_kmp.js
@@ -1,65 +1,68 @@
 export class HeartsRobotKmp {
-    #model;
-    #controller;
-    #position;
-    #todo_list;
-    #doSlowly(fn) {
-        this.#todo_list.push(fn);
-      };
-    
-    
-   
-    
+  #model;
+  #controller;
+  #position;
+  #todo_list;
+  #doSlowly(fn) {
+    this.#todo_list.push(fn);
+  }
 
-    constructor(model, controller, position) {
-        this.#model = model;
-        this.#controller = controller;
-        this.#position = position;
-        this.#todo_list = [];
+  constructor(model, controller, position) {
+    this.#model = model;
+    this.#controller = controller;
+    this.#position = position;
+    this.#todo_list = [];
 
-        setInterval(() => {
-            if (this.#todo_list.length >  0){
-              let next_todo = this.#todo_list.shift();
-              next_todo();
-      
-            }
-          }, 50);
-        
+    setInterval(() => {
+      if (this.#todo_list.length > 0) {
+        let next_todo = this.#todo_list.shift();
+        next_todo();
+      }
+    }, 1000);
 
-        this.#model.addEventListener('stateupdate', () => this.#doSlowly(()=> {
-            let state = this.#model.getState();
-            if ((state == 'passing') && (this.#model.getPassing() != 'none')) {
-                let hand = this.#model.getHand(this.#position);
-                let cards_to_pass = hand.getCards()
-                                        .map(c => [c, Math.random()])
-                                        .sort((a,b) => a[1] - b[1])
-                                        .slice(0, 3)
-                                        .map(cr => cr[0]);
-                this.#controller.passCards(this.#position, cards_to_pass);  
-            } 
-        }));
-        
-        
-        this.#model.addEventListener('trickstart', () => this.#doSlowly(()=>{this.#playCard()}));
-        this.#model.addEventListener('trickplay', () => this.#doSlowly(()=>{this.#playCard()}));
-        
-        
-    }
-    
-
-    #playCard() {
-        if (this.#model.getCurrentTrick().nextToPlay() == this.#position) {
-            let playable_cards = this.#model.getHand(this.#position)
-                                            .getCards()
-                                            .filter(c => this.#controller.isPlayable(this.#position, c));
-            if (playable_cards.length > 0) {
-                let card = playable_cards.map(c => [c, Math.random()])
-                                          .sort((a,b) => a[1] - b[1])[0][0];
-                this.#controller.playCard(this.#position, card);
-            } else {
-                // This should never happen.
-                console.log(`${this.#position} has no playable cards`);
-            }
+    this.#model.addEventListener("stateupdate", () =>
+      this.#doSlowly(() => {
+        let state = this.#model.getState();
+        if (state == "passing" && this.#model.getPassing() != "none") {
+          let hand = this.#model.getHand(this.#position);
+          let cards_to_pass = hand
+            .getCards()
+            .map((c) => [c, Math.random()])
+            .sort((a, b) => a[1] - b[1])
+            .slice(0, 3)
+            .map((cr) => cr[0]);
+          this.#controller.passCards(this.#position, cards_to_pass);
         }
+      })
+    );
+
+    this.#model.addEventListener("trickstart", () =>
+      this.#doSlowly(() => {
+        this.#playCard();
+      })
+    );
+    this.#model.addEventListener("trickplay", () =>
+      this.#doSlowly(() => {
+        this.#playCard();
+      })
+    );
+  }
+
+  #playCard() {
+    if (this.#model.getCurrentTrick().nextToPlay() == this.#position) {
+      let playable_cards = this.#model
+        .getHand(this.#position)
+        .getCards()
+        .filter((c) => this.#controller.isPlayable(this.#position, c));
+      if (playable_cards.length > 0) {
+        let card = playable_cards
+          .map((c) => [c, Math.random()])
+          .sort((a, b) => a[1] - b[1])[0][0];
+        this.#controller.playCard(this.#position, card);
+      } else {
+        // This should never happen.
+        console.log(`${this.#position} has no playable cards`);
+      }
     }
+  }
 }
--- hearts_view2.js
+++ hearts_view2.js
@@ -1,11 +1,10 @@
 import { HeartsRobotKmp } from "./hearts_robot_kmp.js";
 
 //Zhehao Wang helped me achieve a modular rendering style and dynamic button logic (he did not help me "significantly" though)
-//Note: I DID NOT COPY AND PASTE AI CODE OR UTILIZE AI FOR THE ENTIRETY OF THE PROJECT. I used AI to help me figure out ways to model rendering and mapping logic in similar way to w3schools. I mentioned that I used AI regardless in the attestation form
+//Note: I DID NOT COPY AND PASTE AI CODE OR UTILIZE AI FOR THE ENTIRETY OF THE PROJECT. I used AI code examplds to help me figure out ways to model rendering and mapping logic in similar way to w3schools.Regardless, I mentioned that I used AI  in the attestation form.
 //This version is now complete (I submitted a buggy version, and now I am really happy with this one.)
-//audio file is from https://www.videvo.net
+//audio files are from https://www.videvo.net
 //images from https://code.google.com/archive/p/vector-playing-cards/downloads
-//next step is to add animations...
 export class HeartsView {
   model;
   controller;
@@ -15,48 +14,24 @@ export class HeartsView {
     this.model = model;
     this.controller = controller;
     this.playerName = "Saman";
-  
-
 
     this.render(document.querySelector("#main"));
     this.model.addEventListener("stateupdate", () => {
-
-      
-        this.render(document.querySelector("#main"));
-  
-   
-    }
-    )
+      this.render(document.querySelector("#main"));
+    });
     this.model.addEventListener("trickstart", () => {
-
-    
-        this.render(document.querySelector("#main"));
-  
- 
-
-    }  )
+      this.render(document.querySelector("#main"));
+    });
     this.model.addEventListener("trickplay", () => {
-
-
-        this.render(document.querySelector("#main"));
-  
-
-    } )
+      this.render(document.querySelector("#main"));
+    });
     this.model.addEventListener("scoreupdate", (event) => {
-
-        this.handleScoreUpdate(event);
-
-
-    })
+      this.handleScoreUpdate(event);
+    });
   }
 
- 
-
-  
-
   render(renderDiv) {
-    
-      const state = this.model.getState();
+    const state = this.model.getState();
     if (state === "uninitialized") {
       this.renderInitialState(renderDiv);
     } else if (state === "passing") {
@@ -66,7 +41,6 @@ export class HeartsView {
     } else if (state === "complete") {
       this.renderCompleteState(renderDiv);
     }
-    
   }
 
   renderInitialState(renderDiv) {
@@ -79,12 +53,11 @@ export class HeartsView {
     const startGameBtn = renderDiv.querySelector("#startGame");
     startGameBtn.addEventListener("click", () => {
       this.startGame(renderDiv);
-
     });
   }
 
   startGame(renderDiv) {
-    let explosion = new Audio('mlg-airhorn.mp3');
+    let explosion = new Audio("mlg-airhorn.mp3");
     explosion.volume = 1;
     explosion.loop = false;
     explosion.play();
@@ -98,28 +71,29 @@ export class HeartsView {
     this.robot1 = new HeartsRobotKmp(this.model, this.controller, "west");
     this.robot2 = new HeartsRobotKmp(this.model, this.controller, "north");
     this.robot3 = new HeartsRobotKmp(this.model, this.controller, "east");
-    this.robot4 = new HeartsRobotKmp(this.model, this.controller, "south");
+    //this.robot4 = new HeartsRobotKmp(this.model, this.controller, "south");
   }
 
   renderPassingState(renderDiv) {
-    
     // Render passing state UI
     const passingDirection = this.model.getPassing();
     const hand = this.model.getHand("south");
     const cards = hand.getCards();
-  
-    const cardSelectionHTML = cards.map((card, index) => {
-      //choose img based on card
-      return `<div class="card" data-card-index="${index}"><img src="./cards/${card.getRankName()}_of_${card.getSuit()}.png"></div>`
-    }).join("");
-  
+
+    const cardSelectionHTML = cards
+      .map((card, index) => {
+        //choose img based on card
+        return `<div class="card" data-card-index="${index}"><img src="./cards/${card.getRankName()}_of_${card.getSuit()}.png"></div>`;
+      })
+      .join("");
+
     renderDiv.innerHTML = `
       <div id="passingDirection">Passing cards ${passingDirection}</div><br>
       <div id="playerHand" class="hand">${cardSelectionHTML}</div>
       <div id="passCardsMessage" class="message"></div>
       <button id="passCardsButton">Pass Selected Cards</button>
     `;
-    
+
     let selectedCards = [];
     document.querySelectorAll("#playerHand .card").forEach((cardElement) => {
       cardElement.addEventListener("click", () => {
@@ -139,8 +113,7 @@ export class HeartsView {
           }
         }
       });
-    })
-
+    });
 
     document.getElementById("passCardsButton").addEventListener("click", () => {
       if (selectedCards.length === 3) {
@@ -151,39 +124,41 @@ export class HeartsView {
           "<p><b>Please select exactly 3 cards.</b></p>";
       }
     });
-  
-
   }
 
   renderPlayingState(renderDiv) {
     const hand = this.model.getHand("south");
     const cards = hand.getCards();
     const currentTrick = this.model.getCurrentTrick();
-  
-    const cardSelectionHTML = cards.map((card, index) => {
-      //choose img based on card
-      return `<div class="card" data-card-index="${index}"><img src="./cards/${card.getRankName()}_of_${card.getSuit()}.png"></div>`
-    }).join("");
-  
+
+    const cardSelectionHTML = cards
+      .map((card, index) => {
+        //choose img based on card
+        return `<div class="card" data-card-index="${index}"><img src="./cards/${card.getRankName()}_of_${card.getSuit()}.png"></div>`;
+      })
+      .join("");
+
     renderDiv.innerHTML = `
       <div id="playerHand" class="hand">${cardSelectionHTML}</div>
       <button id="cardPlayButton">Play Selected Card</button>
     `;
-  
+
     let trickHTML = '<div id="currentTrick" class="trick">';
     if (currentTrick) {
       let trickCardID = 0;
       ["north", "east", "west", "south"].forEach((position) => {
         const card = currentTrick.getCard(position);
         trickHTML += `<div class="trick-card" id="card${trickCardID++}">${
-          card ? `<img src="./cards/${card.getRankName()}_of_${card.getSuit()}.png">` : `${this.model.getPlayerName(position)} has not gone yet`
+          card
+            ? `<img src="./cards/${card.getRankName()}_of_${card.getSuit()}.png">`
+            : `${this.model.getPlayerName(position)} has not gone yet`
         }</div>`;
       });
     }
     trickHTML += "</div><br>";
     renderDiv.insertAdjacentHTML("beforeend", trickHTML);
-  
-      let selectedCards = [];
+
+    let selectedCards = [];
     document.querySelectorAll("#playerHand .card").forEach((cardElement) => {
       cardElement.addEventListener("click", () => {
         const cardIndex = cardElement.getAttribute("data-card-index");
@@ -193,10 +168,17 @@ export class HeartsView {
           cardElement.classList.remove("selected");
           cardElement.style.fontWeight = "normal";
         } else {
-          if (selectedCards.length < 1 && this.controller.isPlayable("south", selectedCard)) {
+          if (
+            selectedCards.length < 1 &&
+            this.controller.isPlayable("south", selectedCard)
+          ) {
             selectedCards.forEach((idx) => {
-              document.querySelector(`#playerHand .card[data-card-index="${idx}"]`).classList.remove("selected");
-              document.querySelector(`#playerHand .card[data-card-index="${idx}"]`).style.fontWeight = "normal";
+              document
+                .querySelector(`#playerHand .card[data-card-index="${idx}"]`)
+                .classList.remove("selected");
+              document.querySelector(
+                `#playerHand .card[data-card-index="${idx}"]`
+              ).style.fontWeight = "normal";
             });
             selectedCards = [cardIndex];
             cardElement.classList.add("selected");
@@ -205,13 +187,15 @@ export class HeartsView {
         }
       });
     });
-  
+
     document.getElementById("cardPlayButton").addEventListener("click", () => {
       if (selectedCards.length === 0) {
-        document.querySelector("#playerHand").insertAdjacentHTML(
-          "beforeend",
-          `<div><p><b>Please select exactly 1 card</b></p></div>`
-        );
+        document
+          .querySelector("#playerHand")
+          .insertAdjacentHTML(
+            "beforeend",
+            `<div><p><b>Please select exactly 1 card</b></p></div>`
+          );
       }
       if (selectedCards.length === 1) {
         const cardsToPlay = selectedCards.map((index) => cards[index]);
@@ -220,7 +204,6 @@ export class HeartsView {
     });
     this.renderScores(renderDiv);
   }
-  
 
   renderCompleteState(renderDiv) {
     const scores = {
@@ -229,32 +212,41 @@ export class HeartsView {
       [this.playerName]: this.model.getScore("south"),
       walter: this.model.getScore("west"),
     };
-  
+
     const winner = Object.keys(scores).reduce((a, b) =>
       scores[a] < scores[b] ? a : b
     );
-  
-    const scoreDetails = Object.entries(scores).map(([position, score]) => {
-      return `${position.toUpperCase()}: ${score} points`;
-    }).join("<br>");
-  
+
+    const scoreDetails = Object.entries(scores)
+      .map(([position, score]) => {
+        return `${position.toUpperCase()}: ${score} points`;
+      })
+      .join("<br>");
+
     renderDiv.innerHTML = `
       <div>
         <h2>Game Over!</h2>
-        <p>The winner is ${winner.toUpperCase()} with ${scores[winner]} points.</p>
+        <p>The winner is ${winner.toUpperCase()} with ${
+      scores[winner]
+    } points.</p>
         <h3>Final Scores:</h3>
         <p>${scoreDetails}</p>
       </div>`;
-      let explosion = new Audio('explosion.mp3');
-      explosion.volume = 1;
-      explosion.loop = false;
-      explosion.play();
+    let explosion = new Audio("explosion.mp3");
+    explosion.volume = 1;
+    explosion.loop = false;
+    explosion.play();
   }
 
   handleScoreUpdate(event) {
     const { entry, moonshooter } = event.detail;
-    if (moonshooter && ["west", "east", "south", "north"].includes(moonshooter)) {
-      window.alert(`${this.model.getPlayerName(moonshooter)} has shot the moon!`);
+    if (
+      moonshooter &&
+      ["west", "east", "south", "north"].includes(moonshooter)
+    ) {
+      window.alert(
+        `${this.model.getPlayerName(moonshooter)} has shot the moon!`
+      );
     }
   }
 
@@ -267,35 +259,29 @@ export class HeartsView {
     };
 
     let scoreDetails = "<h3>Current Scores:</h3>";
-    scoreDetails += "<table><tr>"
+    scoreDetails += "<table><tr>";
     Object.keys(scores).forEach((name) => {
       scoreDetails += `<th>${name}</th>`;
     });
-    scoreDetails += "</tr>"
-    scoreDetails +="<tr>"
+    scoreDetails += "</tr>";
+    scoreDetails += "<tr>";
     Object.values(scores).forEach((score) => {
       scoreDetails += `<th>Points: ${score}</th>`;
     });
-    scoreDetails += "</tr>"
-    scoreDetails += "</table>"
+    scoreDetails += "</tr>";
+    scoreDetails += "</table>";
 
     // Update the scores section of the UI
     const scoresDiv = renderDiv.querySelector("#scores");
     if (scoresDiv) {
       scoresDiv.innerHTML = scoreDetails;
     } else {
-      renderDiv.insertAdjacentHTML("beforeend", `<div id="scores">${scoreDetails}</div>`);
+      renderDiv.insertAdjacentHTML(
+        "beforeend",
+        `<div id="scores">${scoreDetails}</div>`
+      );
     }
-    //renderDiv.append(scoresDiv);
-  }
-
-  cardimg(rank, suit) {
 
   }
-  
+
 }
-
-  
-
-
-
